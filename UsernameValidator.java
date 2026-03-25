4a4fad8cc016911a7d975e05c1dd21dc9cdb3785
Aniruddha Shastrakar <aniruddhashastrakar1612@gmail.com>
2026-03-25 21:38:07 +0530

    Unit-3 (Que-1)

---
+++ UsernameValidator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class UsernameValidator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String username = sc.nextLine();
+
+        // Remove spaces and convert to lowercase
+        username = username.trim();
+        username = username.toLowerCase();
+
+        System.out.println("Cleaned Username: " + username);
+
+        sc.close();
+    }
+}
