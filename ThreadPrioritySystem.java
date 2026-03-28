b399c44b1babaa9bae5a094faba4b1214fc6a70f
Aniruddha Shastrakar <aniruddhashastrakar1612@gmail.com>
2026-03-28 20:59:33 +0530

    Unit-4 (Que-20)

---
+++ ThreadPrioritySystem.java
@@ -0,0 +1,20 @@
+class ThreadPrioritySystem extends Thread {
+
+    public void run() {
+        System.out.println("Running: " + Thread.currentThread().getName());
+    }
+
+    public static void main(String[] args) {
+        ThreadPrioritySystem t1 = new ThreadPrioritySystem();
+        ThreadPrioritySystem t2 = new ThreadPrioritySystem();
+
+        t1.setName("Thread 1");
+        t2.setName("Thread 2");
+
+        t1.setPriority(Thread.MIN_PRIORITY);
+        t2.setPriority(Thread.MAX_PRIORITY);
+
+        t1.start();
+        t2.start();
+    }
+}
